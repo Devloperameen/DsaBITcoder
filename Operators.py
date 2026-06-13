c39a1159d422957e09f0e128b5ef75ea2281af77
Devloperameen <sadiqferej397@gmail.com>
2026-06-13 07:38:09 +0300

    Initial commit

---
+++ Operators.py
@@ -0,0 +1,16 @@
+# n = int(input("Enter a number: "))
+
+# if n % 2 != 0:
+#     print("Weird")
+# elif 2 <= n <= 5:
+#     print("Not Weird")
+# elif 6 <= n <= 20:
+#     print("Weird")
+# else:
+#     print("Not Weird")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print(a + b)
+print(a - b)
+print(a * b)
